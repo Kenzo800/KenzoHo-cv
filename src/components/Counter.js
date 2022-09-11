ae9174b568fbab4c33cfb0cf6cfac19e9c9526f7
Kenzo <hohochan66@gmail.com>
2022-09-11 21:40:02 +0800

    first test

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+const Counter = () =>{
+    const [count, setCount] = useState(0);
+    return(
+        <div>
+            <p>count: {count}</p>
+            <button onClick={()=> setCount(count + 1)}>+</button>
+        </div>
+    )
+}
+export default Counter;
